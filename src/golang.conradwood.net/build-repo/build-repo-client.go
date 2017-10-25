98626a23f205d578dc4c5a05c2f953adb1c76dc8
Conrad Wood <cnw@singingcat.net>
2017-10-26 00:51:15 +0100

    first file upload successful

--- src/golang.conradwood.net/build-repo/build-repo-client.go
+++ src/golang.conradwood.net/build-repo/build-repo-client.go
@@ -10,7 +10,11 @@ import (
 	"golang.org/x/net/context"
 	//	"net"
 	pb "golang.conradwood.net/build-repo/proto"
+	"io/ioutil"
 	"log"
+	"net"
+	"net/http"
+	"os"
 )
 
 // static variables for flag parser
@@ -21,6 +25,7 @@ var (
 
 func main() {
 	flag.Parse()
+	files := flag.Args()
 	opts := []grpc.DialOption{grpc.WithInsecure()}
 	fmt.Println("Connecting to server...")
 	conn, err := grpc.Dial(*serverAddr, opts...)
@@ -28,17 +33,55 @@ func main() {
 		log.Fatalf("fail to dial: %v", err)
 	}
 	defer conn.Close()
+	ctx := context.Background()
+
 	client := pb.NewBuildRepoManagerClient(conn)
 	req := pb.CreateBuildRequest{
-		Repository: "clientvpn",
+		Repository: "testrepo",
 		CommitID:   "testaccess",
 		Branch:     "master",
 		BuildID:    1,
 		CommitMSG:  "none",
 	}
-	resp, err := client.CreateBuild(context.Background(), &req)
+	resp, err := client.CreateBuild(ctx, &req)
 	if err != nil {
-		log.Fatalf("fail to createvpn: %v", err)
+		log.Fatalf("fail to create build: %v", err)
 	}
-	fmt.Printf("Response to createvpn was: %v\n", resp)
+	fmt.Printf("Response to createbuild was: %v\n", resp)
+	storeid := resp.BuildStoreid
+	serverHost, _, err := net.SplitHostPort(*serverAddr)
+	// why "dog"? I learned of the "range" operator from an example
+	// which uses "dogs" - cnw
+	for _, dog := range files {
+		fmt.Printf("Uploading \"%s\"\n", dog)
+		file, err := os.Open(dog)
+		if err != nil {
+			fmt.Printf("Unable to open \"%s\": %s\n", dog, err)
+			continue
+		}
+		defer file.Close()
+
+		ureq := &pb.UploadSlotRequest{
+			BuildStoreid: storeid,
+			Filename:     dog,
+		}
+		resp, err := client.GetUploadSlot(ctx, ureq)
+		if err != nil {
+			fmt.Printf("Failed to upload %s: %v\n", dog, err)
+			continue
+		}
+		//fmt.Println("Upload Token:", resp)
+		url := fmt.Sprintf("http://%s:%d/upload/%s", serverHost, resp.Port, resp.Token)
+		//fmt.Println("URL: ", url)
+		res, err := http.Post(url, "binary/octet-stream", file)
+		if err != nil {
+			fmt.Println("Failed to upload: ", err)
+			continue
+		}
+
+		defer res.Body.Close()
+		message, _ := ioutil.ReadAll(res.Body)
+		fmt.Printf(string(message))
+
+	}
 }
--- src/golang.conradwood.net/build-repo/build-repo-server.go
+++ src/golang.conradwood.net/build-repo/build-repo-server.go
@@ -4,27 +4,97 @@ import (
 	"fmt"
 	"google.golang.org/grpc"
 	//	"github.com/golang/protobuf/proto"
+	"errors"
 	"flag"
 	pb "golang.conradwood.net/build-repo/proto"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/peer"
+	"io"
 	"log"
+	"math/rand"
 	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
-// static variables for flag parser
+type UploadMetaData struct {
+	Token     string
+	Filename  string
+	Storeid   string
+	Storepath string
+	Created   time.Time
+}
+
+// static variables
 var (
-	port = flag.Int("port", 10000, "The server port")
+	port     = flag.Int("port", 10000, "The server port")
+	base     = "/srv/build-repositories"
+	src      = rand.NewSource(time.Now().UnixNano())
+	uploads  = make(map[string]UploadMetaData)
+	httpPort int
 )
 
+/**************************************************
+* helpers
+***************************************************/
+//https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang
+func RandString(n int) string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	const (
+		letterIdxBits = 6                    // 6 bits to represent a letter index
+		letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+		letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+
+	)
+
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
+
+func FindUploadMetaData(token string) UploadMetaData {
+	umd := uploads[token]
+	umd.Storepath = umd.Storeid
+	return umd
+}
+func StoreUploadMetaData(vfilename string, vtoken string, vstoreid string) {
+	umd := UploadMetaData{
+		Token:    vtoken,
+		Filename: vfilename,
+		Storeid:  vstoreid,
+		Created:  time.Now(),
+	}
+	uploads[vtoken] = umd
+
+}
+
+/**************************************************
+* main entry
+***************************************************/
 func main() {
 	flag.Parse() // parse stuff. see "var" section above
 	listenAddr := fmt.Sprintf(":%d", *port)
-	fmt.Println("Starting VPN Manager service on ", listenAddr)
+	httpPort = *port + 1
 	lis, err := net.Listen("tcp4", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+	http.HandleFunc("/", upload)
 
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
@@ -32,7 +102,35 @@ func main() {
 	s := new(BuildRepoServer)
 	pb.RegisterBuildRepoManagerServer(grpcServer, s) // created by proto
 
+	fmt.Printf("Starting  http service on port %d\n", httpPort)
+	go http.ListenAndServe(fmt.Sprintf(":%d", httpPort), nil)
+
+	fmt.Printf("Starting BuildRepo Manager service on %s\n", listenAddr)
 	grpcServer.Serve(lis)
+
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("HTTP Upload method: %s, content-type: %s\n", r.Method, r.Header.Get("Content-Type"))
+	token := r.URL.Path
+	if !strings.HasPrefix(token, "/upload/") {
+		fmt.Println("Invalid token: ", token)
+		return
+	}
+	token = strings.TrimPrefix(token, "/upload/")
+	//fmt.Printf("Token: \"%s\"\n", token)
+	umd := FindUploadMetaData(token)
+
+	fmt.Printf("Receiving file %s\n", umd.Filename)
+	fname := fmt.Sprintf("%s/%s", umd.Storepath, umd.Filename)
+	f, err := os.OpenFile(fname, os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Printf("Failed to open file %s: %v\n", fname, err)
+		return
+	}
+	defer f.Close()
+	io.Copy(f, r.Body)
+
 }
 
 /**********************************
@@ -51,11 +149,38 @@ func (s *BuildRepoServer) CreateBuild(ctx context.Context, CreateRequest *pb.Cre
 	if !ok {
 		fmt.Println("Error getting peer ")
 	}
-	fmt.Println(peer.Addr, "called createvpn")
 	resp := pb.CreateBuildResponse{}
+	dir := fmt.Sprintf("%s/%s/%s/%d", base, CreateRequest.Repository, CreateRequest.Branch, CreateRequest.BuildID)
+	err := os.MkdirAll(dir, 0777)
+	if err != nil {
+		fmt.Println("Failed to create directory ", dir, err)
+		return &resp, err
+	}
+	fmt.Println(peer.Addr, "called createbuild")
+	resp.BuildStoreid = dir
 	return &resp, nil
 }
 
+func (s *BuildRepoServer) GetUploadSlot(ctx context.Context, pr *pb.UploadSlotRequest) (*pb.UploadSlotResponse, error) {
+
+	res := &pb.UploadSlotResponse{}
+	storeid := pr.BuildStoreid
+	fname := pr.Filename
+	fname = filepath.Clean(fname)
+	if filepath.IsAbs(fname) {
+		return res, errors.New("file must be relative")
+	}
+	if !strings.HasPrefix(storeid, base) {
+		fmt.Printf("Base=\"%s\", but token sent was: \"%s\"\n", base, storeid)
+		return res, errors.New("storeid is invalid")
+	}
+	fmt.Printf("Request to upload file \"%s\" to store \"%s\"\n", fname, storeid)
+	token := RandString(256)
+	res.Token = token
+	res.Port = int32(httpPort)
+	StoreUploadMetaData(fname, token, storeid)
+	return res, nil
+}
 func (s *BuildRepoServer) Ping(ctx context.Context, pr *pb.PingRequest) (*pb.PingResponse, error) {
 	fmt.Println("pong")
 	return nil, nil
