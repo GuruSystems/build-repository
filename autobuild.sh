f6c06bcc5d2d74843ed6dc5e7da5ef773580d21e
Conrad Wood <cnw@singingcat.net>
2017-11-06 20:53:27 +0000

    build for i386 and amd64

--- autobuild.sh
+++ autobuild.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 # go insists on absolute path.
-export GOBIN=`pwd`/dist
 export GOPATH=`pwd`
 echo "GOPATH=$GOPATH"
-mkdir $GOBIN
+
+buildall() {
+mkdir -p $GOBIN
 MYSRC=src/golang.conradwood.net/build-repo/
 ( cd ${MYSRC} && make proto ) || exit 10
 ( cd ${MYSRC} && make client ) || exit 10
 ( cd ${MYSRC} && make server ) || exit 10
 cp -rvf ${MYSRC}/proto dist/
+}
+
+export GOBIN=`pwd`/dist/i386
+buildall
+export GOBIN=`pwd`/dist/amd64
+GOOS=linux
+GOARCH=amd64
+
+buildall
 
 cp dist/build-repo-client /usr/local/bin/
 build-repo-client -branch=${GIT_BRANCH} -build=${BUILD_NUMBER} -commitid=${COMMIT_ID} -commitmsg="commit msg unknown" -repository=${PROJECT_NAME} -server_addr=buildrepo:5004 
