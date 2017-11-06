0113f3fd3cec528f80063b8133c1b4e55af60988
Conrad Wood <cnw@singingcat.net>
2017-11-06 20:33:49 +0000

    .

--- autobuild.sh
+++ autobuild.sh
@@ -11,6 +11,6 @@ MYSRC=src/golang.conradwood.net/build-repo/
 cp -rvf ${MYSRC}/proto dist/
 
 cp dist/build-repo-client /usr/local/bin/
-build-repo-client -branch=${GIT_BRANCH} -build=${BUILD_NUMBER} -commitid=${COMMIT_ID} -commitmsg="commit msg unknown" -repository=${PROJECT_NAME} -server_addr=buildrepo:5004 dist/*
+build-repo-client -branch=${GIT_BRANCH} -build=${BUILD_NUMBER} -commitid=${COMMIT_ID} -commitmsg="commit msg unknown" -repository=${PROJECT_NAME} -server_addr=buildrepo:5004 
 
 exit 0
