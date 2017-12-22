b1f4f7d713442b8f2da010b8b45b468e992aaf4b
Conrad Wood <cnw@singingcat.net>
2017-12-22 13:56:32 +0000

    .

--- autobuild.sh
+++ autobuild.sh
@@ -12,14 +12,12 @@ MYSRC=src/golang.conradwood.net/build-repo/
 cp -rvf ${MYSRC}/proto dist/
 }
 
-export GOBIN=`pwd`/dist/i386
-buildall
 export GOBIN=`pwd`/dist/amd64
 export GOOS=linux
 export GOARCH=amd64
 buildall
 
-cp -v dist/i386/build-repo-client /usr/local/bin/ || exit 10
+cp -v dist/amd64/build-repo-client /usr/local/bin/ || exit 10
 build-repo-client -branch=${GIT_BRANCH} -build=${BUILD_NUMBER} -commitid=${COMMIT_ID} -commitmsg="commit msg unknown" -repository=${PROJECT_NAME} -server_addr=buildrepo:5004 
 
 exit 0
