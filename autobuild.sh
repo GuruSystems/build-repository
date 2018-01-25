fb4050fc5380a9aa7d9dd9b70721a956d673a887
Conrad Wood <cnw@singingcat.net>
2018-01-25 12:42:49 +0000

    build for mac os as well

--- autobuild.sh
+++ autobuild.sh
@@ -4,18 +4,28 @@ export GOPATH=`pwd`
 echo "GOPATH=$GOPATH"
 
 buildall() {
-mkdir -p $GOBIN
-MYSRC=src/golang.conradwood.net/build-repo/
-( cd ${MYSRC} && make proto ) || exit 10
-( cd ${MYSRC} && make client ) || exit 10
-( cd ${MYSRC} && make server ) || exit 10
-cp -rvf ${MYSRC}/proto dist/
+    GOBIN=GOPATH/dist/${GOOS}/${GOARCH}/
+    mkdir -p $GOBIN
+    MYSRC=src/golang.conradwood.net/build-repo/
+    ( cd ${MYSRC} && make proto ) || exit 10
+    ( cd ${MYSRC} && make client ) || exit 10
+    ( cd ${MYSRC} && make server ) || exit 10
+    cp -rvf ${MYSRC}/proto dist/
 }
 
-export GOBIN=`pwd`/dist/amd64
+GOBIN=GOPATH/dist/proto
+( cd ${MYSRC} && make proto ) || exit 10
+
+#========= build linux
 export GOOS=linux
 export GOARCH=amd64
 buildall
+
+#========= build mac
+export GOOS=darwin
+export GOARCH=386
+buildall
+
 
 cp -v dist/amd64/build-repo-client /usr/local/bin/ || exit 10
 build-repo-client -branch=${GIT_BRANCH} -build=${BUILD_NUMBER} -commitid=${COMMIT_ID} -commitmsg="commit msg unknown" -repository=${PROJECT_NAME} -server_addr=buildrepo:5004 
