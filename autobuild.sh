9628d1a9cfa029cf679912dbd379a382ff2236bb
AHB <alex@AHBs-MacBook-Pro.local>
2018-02-19 10:16:43 +0000

    fix autobuild.sh

--- autobuild.sh
+++ autobuild.sh
@@ -2,34 +2,38 @@
 # go insists on absolute path.
 export GOPATH=`pwd`
 echo "GOPATH=$GOPATH"
-export MYSRC=src/golang.conradwood.net/build-repo/
 
 rm -rf dist
 
 buildall() {
+    echo "Building ${GOOS}/${GOARCH}"
     export GOBIN=$GOPATH/dist/${GOOS}/${GOARCH}/
     mkdir -p $GOBIN
-    ( cd ${MYSRC} && make proto ) || exit 10
-    ( cd ${MYSRC} && make client ) || exit 10
-    ( cd ${MYSRC} && make server ) || exit 10
-    cp -rvf ${MYSRC}/proto dist/
+    mkdir -p dist/${GOOS}/${GOARCH}/ || exit 10
+    ( cd client && go build -o ../dist/${GOOS}/${GOARCH}/build-repo-client ) || exit 10
+    ( cd server && go build -o ../dist/${GOOS}/${GOARCH}/build-repo-server) || exit 10
 }
 
 GOBIN=GOPATH/dist/proto
-( cd ${MYSRC} && make proto ) || exit 10
 
 #========= build linux
 export GOOS=linux
 export GOARCH=amd64
 buildall
+mkdir -p dist/${GOOS}/${GOARCH}/ || exit 10
+cp -p client/build-repo* dist/${GOOS}/${GOARCH}/ || exit 10
 
 #========= build mac
 export GOOS=darwin
 export GOARCH=386
 buildall
 
-
 cp -v dist/linux/amd64/build-repo-client /usr/local/bin/ || exit 10
-build-repo-client -branch=${GIT_BRANCH} -build=${BUILD_NUMBER} -commitid=${COMMIT_ID} -commitmsg="commit msg unknown" -repository=${PROJECT_NAME} -server_addr=buildrepo:5004 
+
+echo "Done OK"
+
+build-repo-client -branch=${GIT_BRANCH} -build=${BUILD_NUMBER} -commitid=${COMMIT_ID} -commitmsg="commit msg unknown" -repository=${PROJECT_NAME} -server_addr=buildrepo:5004
+
+
 
 exit 0
